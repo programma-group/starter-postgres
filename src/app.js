17f347cfcff87499ce411441169512d01f1ec972
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-11-09 18:40:46 -0400

    Fixing eslint style

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
-const app = express();
 
+const app = express();
 app.use(helmet());
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.json('hello world')
+  res.json('hello world');
 });
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const app = require('./app');
+
 const port = process.env.PORT || 3000;
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening on port ${port}`));
