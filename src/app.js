62a837e6679bbb5c15387f1fa0be0910914126fc
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-11-09 17:38:45 -0400

    Added hello world example

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.json('hello world')
+});
+
+module.exports = app;
