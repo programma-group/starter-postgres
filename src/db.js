da1abfd59b2b22116e55a80b1b7617033ee34b9a
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-11-10 18:08:39 -0400

    Required knexConfig file on src/db.js

--- src/db.js
+++ src/db.js
@@ -1,24 +1,8 @@
 const knex = require('knex');
 const { Model } = require('objection');
+const knexConfig = require('./knexfile');
 
-const {
-  DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_MIN_POOL, DB_MAX_POOL, DB_PORT,
-} = process.env;
-
-const knexObject = knex({
-  client: 'postgresql',
-  connection: {
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
-    port: DB_PORT,
-  },
-  pool: {
-    min: parseInt(DB_MIN_POOL, 10),
-    max: parseInt(DB_MAX_POOL, 10),
-  },
-});
+const knexObject = knex(knexConfig.development);
 
 const init = () => Model.knex(knexObject);
 
