feaf859c9eb8f2f7e683642339708123e1797802
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-11-15 06:04:29 -0400

    Added middlewares to app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const initDb = require('./db');
+const userRoute = require('./routes/user');
 
 initDb();
 
 const app = express();
 app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/user', userRoute);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
