b9d50943c3709a9a3f81cabd49f1629c96fde23f
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-11-17 10:16:02 -0400

    Seeding and truncating databases

--- test/init.js
+++ test/init.js
@@ -6,10 +6,16 @@ require('dotenv').config({
 });
 
 const { init: initDbConnection, knexObject } = require('../src/db');
+const { seedTestDatabase, truncateTables } = require('./utils/db');
 
 beforeAll(async () => {
   await initDbConnection();
   await knexObject.migrate.latest({
     directory: path.resolve(__dirname, '..', 'src', 'migrations'),
   });
+  await seedTestDatabase(knexObject);
+});
+
+afterAll(async () => {
+  await truncateTables(knexObject);
 });
