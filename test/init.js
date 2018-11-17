1570e3792a71dec9b7d3f8fb3953cf41a0a0d67c
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-11-17 09:45:12 -0400

    Adding init of Db connection for tests

--- test/init.js
+++ test/init.js
@@ -4,3 +4,12 @@ const envPath = path.resolve(__dirname, '..', '.env.test');
 require('dotenv').config({
   path: envPath,
 });
+
+const { init: initDbConnection, knexObject } = require('../src/db');
+
+beforeAll(async () => {
+  await initDbConnection();
+  await knexObject.migrate.latest({
+    directory: path.resolve(__dirname, '..', 'src', 'migrations'),
+  });
+});
