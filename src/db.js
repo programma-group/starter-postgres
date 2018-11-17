67d89766176a5e147e115c6aae74619f79b501b3
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-11-17 09:45:25 -0400

    Exporting knexObject and made changes on init

--- src/db.js
+++ src/db.js
@@ -4,6 +4,16 @@ const knexConfig = require('./knexfile');
 
 const knexObject = knex(knexConfig.development);
 
-const init = () => Model.knex(knexObject);
+/**
+ * Creates a connection and tests it, according to https://github.com/tgriesser/knex/issues/1886
+ * @returns {Promise} resolved if init was succesful
+ */
+const init = () => {
+  Model.knex(knexObject);
+  return knexObject.raw('select 1+1 as result');
+};
 
-module.exports = init;
+module.exports = {
+  init,
+  knexObject,
+};
