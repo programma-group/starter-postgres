96b2bfe9ebdb52d899f984c221bc31607659f524
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-11-17 14:40:04 -0400

    Added tests for validations for users

--- test/endpoints/user/postUser.test.js
+++ test/endpoints/user/postUser.test.js
@@ -1,12 +1,20 @@
 const request = require('supertest');
 const app = require('../../../src/app');
-const { getCorrectUserData } = require('../../utils/user');
+const { getCorrectUserData, getOmittedData } = require('../../utils/user');
 
 const goodRequest = (body = {}) => (
   request(app)
     .post('/user')
     .send(body)
 );
+
+const requiredFields = [
+  'username',
+  'password',
+  'email',
+  'firstName',
+  'lastName',
+];
 
 describe('POST /user', () => {
   it('correctly posts a user', async () => {
@@ -23,5 +31,17 @@ describe('POST /user', () => {
         }));
       })
       .expect(200);
+  });
+  requiredFields.forEach((field) => {
+    const data = getOmittedData(field);
+    it(`should return error if ${field} is not present on the payload`, async () => {
+      await goodRequest(data)
+        .expect(({ body }) => {
+          expect(body.ok).toBe(false);
+          expect(body.type).toBe('ModelValidation');
+          expect(body.data).toMatchSnapshot(field);
+        })
+        .expect(400);
+    });
   });
 });
