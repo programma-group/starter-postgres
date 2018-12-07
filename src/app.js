41e2a68d4e4e2dde1b13cb9675c3482398ddbd1e
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-12-07 15:20:43 -0400

    Added auth routes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { init: initDbConnection } = require('./db');
-const userRoute = require('./routes/user');
+const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
+const User = require('./models/user');
 
 initDbConnection().catch((err) => {
   // eslint-disable-next-line no-console
@@ -12,10 +14,12 @@ initDbConnection().catch((err) => {
 });
 
 const app = express();
+app.set('models.user', User);
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/user', userRoute);
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
