326a8f307536f89c90bef0c0c08bd3b570096054
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-12-12 16:30:24 -0400

    Added passwordConfirm to getCorrectUserData method of user test utils

--- test/utils/user.js
+++ test/utils/user.js
@@ -1,6 +1,7 @@
 const getCorrectUserData = () => ({
   username: 'inserted_user',
   password: '123456',
+  passwordConfirm: '123456',
   email: 'inserted_user@admin.com',
   firstName: 'Inserted',
   lastName: 'User',
