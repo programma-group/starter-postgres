1b3a5f175d75971214b9d9d94b02567c65ae22b0
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-12-12 17:24:30 -0400

    Testing models in database

--- test/endpoints/auth/register.test.js
+++ test/endpoints/auth/register.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../../../src/app');
+const User = require('../../../src/models/user');
 const { getCorrectUserData, getOmittedData } = require('../../utils/user');
 
 const makeRequest = (body = {}) => (
@@ -19,7 +20,8 @@ const requiredFields = [
 
 describe('POST /auth/register', () => {
   it('should register a user', async () => {
-    await makeRequest(getCorrectUserData())
+    const data = getCorrectUserData();
+    await makeRequest(data)
       .expect((res) => {
         expect(res.body).toMatchObject(expect.objectContaining({
           ok: true,
@@ -27,11 +29,22 @@ describe('POST /auth/register', () => {
             token: expect.any(String),
           },
         }));
-      });
+      })
+      .expect(200);
+    const c = await User.query().where('email', data.email).count();
+    expect(c[0].count).toBe('1');
+    const userObj = await User.query().where('email', data.email).first();
+    expect(userObj).toMatchObject(expect.objectContaining({
+      email: data.email,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      username: data.username,
+    }));
   });
   requiredFields.forEach((field) => {
     const data = getOmittedData(field);
     it(`should return error if ${field} is not present on the payload`, async () => {
+      const initialCount = await User.query().count();
       await makeRequest(data)
         .expect((res) => {
           expect(res.body).toMatchObject(expect.objectContaining({
@@ -42,6 +55,8 @@ describe('POST /auth/register', () => {
           expect(res.body.errors).toMatchSnapshot(`${field} errors`);
         })
         .expect(400);
+      const finalCount = await User.query().count();
+      expect(initialCount[0].count).toBe(finalCount[0].count);
     });
   });
 });
