2b67cb38bac0bf803244a47a5f58cd34e9776c05
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2018-12-19 15:25:41 -0400

    Added route for docs

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 const passport = require('./passport');
 const { init: initDbConnection } = require('./db');
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/auth');
 const User = require('./models/user');
 const mail = require('./utils/mail');
+const swaggerSpec = require('./utils/swagger');
 
 initDbConnection().catch((err) => {
   // eslint-disable-next-line no-console
@@ -23,6 +25,9 @@ app.use(helmet());
 app.use(cors());
 app.use(passport.initialize());
 app.use(bodyParser.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  explorer: true,
+}));
 app.use('/user', authMiddleware, userRouter);
 app.use('/auth', authRouter);
 
