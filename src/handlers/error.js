323a6778ce2ad0aad552125d281c0e6fd4d4f40a
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-01-06 13:10:04 -0400

    Refactored error handler

--- src/handlers/error.js
+++ src/handlers/error.js
@@ -1,132 +1,22 @@
-const {
-  ValidationError,
-  NotFoundError,
-} = require('objection');
+const { ValidationError } = require('objection');
+const serializeError = require('serialize-error');
 
-const {
-  DBError,
-  UniqueViolationError,
-  NotNullViolationError,
-  ForeignKeyViolationError,
-  CheckViolationError,
-  DataError,
-} = require('objection-db-errors');
+const sendErrorMessage = (res, status, message, type, data) => (
+  res.status(status).send({
+    ok: false,
+    message,
+    type,
+    data,
+  })
+);
 
-function errorHandler(err, res) {
-  if (err instanceof ValidationError) {
-    switch (err.type) {
-      case 'ModelValidation':
-        res.status(400).send({
-          ok: false,
-          message: err.message,
-          type: 'ModelValidation',
-          data: err.data,
-        });
-        break;
-      case 'RelationExpression':
-        res.status(400).send({
-          ok: false,
-          message: err.message,
-          type: 'InvalidRelationExpression',
-          data: {},
-        });
-        break;
-      case 'UnallowedRelation':
-        res.status(400).send({
-          ok: false,
-          message: err.message,
-          type: 'UnallowedRelation',
-          data: {},
-        });
-        break;
-      case 'InvalidGraph':
-        res.status(400).send({
-          ok: false,
-          message: err.message,
-          type: 'InvalidGraph',
-          data: {},
-        });
-        break;
-      default:
-        res.status(400).send({
-          ok: false,
-          message: err.message,
-          type: 'UnknownValidationError',
-          data: {},
-        });
-        break;
-    }
-  } else if (err instanceof NotFoundError) {
-    res.status(404).send({
-      ok: false,
-      message: err.message,
-      type: 'NotFound',
-      data: {},
-    });
-  } else if (err instanceof UniqueViolationError) {
-    res.status(409).send({
-      ok: false,
-      message: err.message,
-      type: 'UniqueViolation',
-      data: {
-        columns: err.columns,
-        table: err.table,
-        constraint: err.constraint,
-      },
-    });
-  } else if (err instanceof NotNullViolationError) {
-    res.status(400).send({
-      ok: false,
-      message: err.message,
-      type: 'NotNullViolation',
-      data: {
-        column: err.column,
-        table: err.table,
-      },
-    });
-  } else if (err instanceof ForeignKeyViolationError) {
-    res.status(409).send({
-      ok: false,
-      message: err.message,
-      type: 'ForeignKeyViolation',
-      data: {
-        table: err.table,
-        constraint: err.constraint,
-      },
-    });
-  } else if (err instanceof CheckViolationError) {
-    res.status(400).send({
-      ok: false,
-      message: err.message,
-      type: 'CheckViolation',
-      data: {
-        table: err.table,
-        constraint: err.constraint,
-      },
-    });
-  } else if (err instanceof DataError) {
-    res.status(400).send({
-      ok: false,
-      message: err.message,
-      type: 'InvalidData',
-      data: {},
-    });
-  } else if (err instanceof DBError) {
-    res.status(500).send({
-      ok: false,
-      message: err.message,
-      type: 'UnknownDatabaseError',
-      data: {},
-    });
-  } else {
-    res.status(500).send({
-      ok: false,
-      message: err.message,
-      type: 'UnknownError',
-      data: {},
-    });
+const errorHandler = (err, res) => {
+  if (err instanceof ValidationError && err.type === 'ModelValidation') {
+    return sendErrorMessage(res, 400, err.message, 'ModelValidation', err.data);
   }
-}
+  const errorData = process.env.NODE_ENV === 'development' ? serializeError(err) : {};
+  return sendErrorMessage(res, 500, err.message, err.constructor.name || 'UnknownError', errorData);
+};
 
 // eslint-disable-next-line consistent-return
 const catchErrors = fn => async (req, res, next) => {
